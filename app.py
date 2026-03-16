9ca7dbabc097c523f3f66d7a75d5f06c08aa3efb
Dhrubo26 <www.druborangsa26@gmail.com>
2026-03-17 01:08:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ database_url = os.getenv("DATABASE_URL")
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url or local_db
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:123456@localhost/blood_bank_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
